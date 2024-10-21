35343f24ce18a1c761af092cec548fffded716e6
Saumyasharma974 <sharmasaumya2464@gmail.com>
2024-10-21 13:14:25 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Fragment } from 'react'
+
+function App() {
+  let a=10;
+  return (
+    <Fragment>
+      <h1>i m learning {a} react</h1>
+    </Fragment>
+  )
+}
+
+export default App
